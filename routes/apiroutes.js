7ee29cf95894fcd9215ac10b0e5dc9a4c2203d00
Amy Paschke <pasch049@umn.edu>
2020-12-13 16:03:36 -0600

    changed a few connections

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,7 +1,12 @@
 const dataBase = require("../db/db.json");
-const app = express();
+
 const express = require("express");
 const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// const js = require("../public/assets/js/index");
 
 module.exports = function (app) {
   app.get("/api/notes", (req, res) => {
@@ -9,8 +14,25 @@ module.exports = function (app) {
   });
 };
 
-app.post("/api/notes", (req, res) => {
+app.post("/notes", (req, res) => {
   if (dataBase.length) {
     dataBase.push(req.body);
+    res.json(true);
   }
 });
+
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+// const dataBase = require("../db/notes.js");
+// module.exports = function (app) {
+//   app.get("/api/notes", (req, res) => {
+//     res.json(dataBase);
+//   });
+// };
+// app.post("/notes", (req, res) => {
+//   if (dataBase) {
+//     dataBase.push(req.body);
+//     res.json(true);
+//   }
+// });
--- server.js
+++ server.js
@@ -1,24 +1,13 @@
+// require express npm packages to utilize for server functionality
 const express = require("express");
-const path = require("path");
-
+// save the express server into a variable
 const app = express();
+//Sets an initial port.
 const PORT = process.env.PORT || 3000;
-
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "./public")));
-
-// require("./routes/htmlroutes")(app);
-// require("./routes/apiroutes");
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./notes.html"));
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./index.html"));
-});
-
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes")(app);
+//Starts up our Server
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));

3d58cfdaeb9c161e1e992bfd49f45a338216ad11
Amy Paschke <pasch049@umn.edu>
2020-12-14 20:52:19 -0600

    rewrote main server page and deleted multiple files

--- db/notes.js
+++
@@ -1,3 +0,0 @@
-const notesArray = [];
-
-module.exports = notesArray;
--- server.js
+++ server.js
@@ -1,13 +1,76 @@
-// require express npm packages to utilize for server functionality
 const express = require("express");
-// save the express server into a variable
+const path = require("path");
+const fs = require("fs");
+
 const app = express();
-//Sets an initial port.
 const PORT = process.env.PORT || 3000;
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+
+let notesArray = [];
+
 app.use(express.json());
-require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes")(app);
-//Starts up our Server
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("/api/notes", function (err, res) {
+  try {
+    notesArray = fs.readFileSync("./db/db.json", "utf8");
+    console.log("Processing request");
+    notesArray = JSON.parse(notesArray);
+  } catch (error) {
+    console.log("There was an error with this process.");
+    console.log(error);
+  }
+  res.json(notesArray);
+});
+
+// we use this function to write a "note" like with writing a new file in previous examples
+app.post("/api/notes", function (req, res) {
+  try {
+    notesArray = fs.readFileSync("./db/db.json", "utf8");
+    console.log(notesArray);
+    notesArray = JSON.parse(notesArray);
+    req.body.id = notesArray.length;
+    notesArray.push(req.body);
+    notesArray = JSON.stringify(notesArray); //we have to stringify the array so the page can read it
+    fs.writeFile("./db/db.json", notesArray, "utf8", function (error) {
+      if (error) throw error;
+    });
+    res.json(JSON.parse(notesArray));
+  } catch (error) {
+    throw error;
+  }
+});
+
+//function for deleting an entered note aside from the test note
+app.delete("/api/notes/:id", function (req, res) {
+  try {
+    notesArray = fs.readFileSync("./db/db.json", "utf8");
+    notesArray = JSON.parse(notesArray);
+    notesArray = notesArray.filter(function (note) {
+      return note.id != req.params.id;
+    });
+    notesArray = JSON.stringify(notesArray);
+    fs.writeFile("./db/db.json", notesArray, "utf8", function (err) {
+      if (err) throw err;
+    });
+    res.send(JSON.parse(notesArray));
+  } catch (error) {
+    throw error;
+  }
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  return res.sendFile(path.json(__dirname, "/db/db.json"));
+});
+
+app.listen(PORT, function () {
+  console.log("SERVER IS LISTENING: " + PORT);
+});
